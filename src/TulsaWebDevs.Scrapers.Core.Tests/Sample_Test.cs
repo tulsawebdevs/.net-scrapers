562e672afd591e707f56934773364dd158471702
Chris Patterson <chris@phatboyg.com>
2012-02-25 11:33:52 -0600

    First commit

---
+++ src/TulsaWebDevs.Scrapers.Core.Tests/Sample_Test.cs
@@ -0,0 +1,14 @@
+﻿namespace TulsaWebDevs.Scrapers.Core.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Sample_Test
+    {
+        [Test]
+        public void A_sample_test_that_will_pass_with_flying_colors()
+        {
+            Assert.AreEqual(12.45m, 12.45m);
+        }
+    }
+}
